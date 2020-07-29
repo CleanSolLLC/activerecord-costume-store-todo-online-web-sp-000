693c2a8709ef9fef2027a9aabeee8d6b135ae360
Mark May <mamark90@gmail.com>
2020-07-29 22:38:27 +0000

    Automatically backed up by Learn

--- db/migrate/007_change_costume_store_column_name.rb
+++ db/migrate/007_change_costume_store_column_name.rb
@@ -3,7 +3,7 @@ class ChangeCostumeStoreColumnName < ActiveRecord::Migration[4.2]
   def change 
     change_table :costume_stores do |t|
       t.rename :start_time, :opening_time
-      t.rename :
+      t.rename :end_time, :closing_time
     end
   end
   
