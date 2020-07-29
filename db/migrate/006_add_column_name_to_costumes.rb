f44ddc1baf89815d9097cdb0d0370c9c582e9f63
Mark May <mamark90@gmail.com>
2020-07-29 22:33:50 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_column_name_to_costumes.rb
+++ db/migrate/006_add_column_name_to_costumes.rb
@@ -2,7 +2,7 @@ class AddColumnNameToCostumes < ActiveRecord::Migration[4.2]
 
 
   def change 
-    add_column :costumes, :start_time, :timestamp
+    add_column :costumes, :created_at, :timestamp
     add_column :costumes, :updated_at, :timestamp
   end
       
